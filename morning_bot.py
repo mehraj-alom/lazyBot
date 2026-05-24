9f644158b532990f0224570c7d25d2403dbfa502
Mehraj Alom Tapadar <mehraj.official000@gmail.com>
2026-05-24 23:30:19 +0530

    final

---
+++ morning_bot.py
@@ -0,0 +1,88 @@
+"""
+AI Telegram Motivation Bot
+"""
+
+import os
+import requests
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+
+load_dotenv()
+
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=OPENAI_API_KEY,
+)
+
+
+with open("context.txt", "r") as file:
+    context = file.read()
+
+
+prompt = f"""
+{context}
+
+Generate ONE powerful evening Telegram message.
+
+Style:
+- personal
+- emotionally intelligent
+- ambitious
+- slightly romantic/warm
+- intense
+- deeply motivating
+- human
+- a quote that is romentic and motivating
+
+Keep under 100 words.
+
+Add emojis naturally.
+"""
+
+
+response = client.chat.completions.create(
+    model="openai/gpt-oss-120b:free",
+    messages=[
+        {
+            "role": "system",
+            "content": prompt
+        }
+    ]
+)
+
+
+message = response.choices[0].message.content
+
+
+telegram_url = (
+    f"https://api.telegram.org/bot"
+    f"{TELEGRAM_BOT_TOKEN}/sendMessage"
+)
+
+
+payload = {
+    "chat_id": TELEGRAM_CHAT_ID,
+    "text": message
+}
+
+
+requests.post(
+    telegram_url,
+    json=payload
+)
+
+
+print("\nMESSAGE SENT:\n")
+
+print(message)
